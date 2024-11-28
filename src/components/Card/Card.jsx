b9bc68b66e103c32f7309a04255a38f5dc28ed72
Mohsin Rahman <mohsinrahmang@gmail.com>
2024-11-28 03:27:39 +0100

    redux value

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import Chip from '../../assets/chip-light.svg';
+import chiplight from '../../assets/chip-light.svg';
 import '../../styles/globalStyle.scss';
 import './Card.scss';
 
 function Card({ cardData, onClick }) {
-    console.log('Card data:', cardData);
-
     const { vendor } = cardData
     
-    const cardNumberDisplay = cardData && cardData.number ? cardData.number : 'XXXX XXXX XXXX XXXX';
+    const cardNumber = cardData && cardData.number ? cardData.number : 'XXXX XXXX XXXX XXXX';
     const cardHolderName = cardData && cardData.cardholder ? cardData.cardholder : 'FIRSTNAME LASTNAME';
     const validThru = cardData && cardData.expiry ? cardData.expiry : 'MM/YY'
 
     return (
         <main className='main-card'>
             <div className={`card ${vendor.toLowerCase()} vendor-style`} onClick={onClick}>
-                <img className='card__chip' src={Chip} alt='chip' />
-                <p className='card__number'>{cardNumberDisplay}</p>
+                <img className='card__chip' src={chiplight} alt='chiplight' />
+                <p className='card__number'>{cardNumber}</p>
                 <div className='card__information'>
                     <aside className='card__information__left'>
                         <p className='card__information__cardholder-name'>CARDHOLDER NAME </p>
